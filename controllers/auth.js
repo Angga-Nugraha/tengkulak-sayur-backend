126b722611a15a23d59789862b34b4a621124c3d
Angga Nugraha <angga250496@gmail.com>
2022-12-10 23:20:19 +0700

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -86,7 +86,6 @@ export const register = async (req, res) => {
         });
         res.status(200).send({ msg: "Register berhasil", data: user });
     } catch (error) {
-        res.status(400).json({ msg: "Email sudah terdaftar" });
         console.log(error);
     }
 }
