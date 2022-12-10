f21c8dcaafa6d17f74851748850316f4bd62865f
Angga Nugraha <angga250496@gmail.com>
2022-12-10 18:36:53 +0700

    update

--- config/database.js
+++ config/database.js
@@ -1,10 +1,11 @@
 import { Sequelize } from "sequelize";
 
 
-const db = new Sequelize('tengkulak_db', 'root', '', {
-    host: '127.0.0.1',
-    dialect: 'mysql',
-});
+const db = new Sequelize(process.env.DB_URL, { dialect: 'mysql' });
 
+// 'tengkulak_db', 'root', '', {
+//     host: '127.0.0.1',
+//     dialect: 'mysql',
+// }
 
 export default db
